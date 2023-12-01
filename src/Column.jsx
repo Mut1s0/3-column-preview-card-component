5f49bafd00c8365f0ac5d4de973de27cb00a3638
Allan Musyoka <musyokaallan96@gmail.com>
2023-12-01 15:29:25 +0300

    Added mobile styling

--- src/Column.jsx
+++ src/Column.jsx
@@ -7,13 +7,13 @@ function Column() {
 
     return(
 
-        <main className='h-screen flex justify-center items-center'>
+        <main className='h-[1200px] flex justify-center items-center'>
 
-            <div>
+            <div className="container max-w-[85%] w-[600px]">
 
-                <section>
+                <section className="py-9 px-9 rounded-t-lg">
 
-                    <header>
+                    <header className="pb-8">
 
                         <img src={Sedans} alt="Sedans" />
 
@@ -21,23 +21,23 @@ function Column() {
 
                     <div>
 
-                        <h1>Sedans</h1>
+                        <h1 className="text-[30px] uppercase pb-6">Sedans</h1>
 
-                        <p>Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.</p>
+                        <p className="text-[12px]">Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.</p>
 
                     </div>
 
-                    <footer>
+                    <footer className="pt-10">
 
-                        <button>Learn More</button>
+                        <button className="py-2 px-5 rounded-full text-[12px]">Learn More</button>
 
                     </footer>
 
                 </section>
 
-                <section>
+                <section className="py-9 px-9">
 
-                    <header>
+                    <header className="pb-8">
 
                         <img src={SUVS} alt="SUVS" />
 
@@ -45,23 +45,23 @@ function Column() {
 
                     <div>
 
-                        <h1>SUVs</h1>
+                        <h1 className="text-[38px] uppercase pb-6">SUVs</h1>
 
-                        <p>Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures.</p>
+                        <p className="text-[12px]">Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures.</p>
 
                     </div>
 
-                    <footer>
+                    <footer className="pt-10">
 
-                        <button>Learn More</button>
+                        <button className="py-2 px-5 rounded-full text-[12px]">Learn More</button>
 
                     </footer>
 
                 </section>
 
-                <section>
+                <section className="py-9 px-9 rounded-b-lg">
 
-                    <header>
+                    <header className="pb-8">
 
                         <img src={Luxury} alt="Luxury" />
 
@@ -69,15 +69,15 @@ function Column() {
 
                     <div>
 
-                        <h1>Luxury</h1>
+                        <h1 className="text-[28px] uppercase pb-6">Luxury</h1>
 
-                        <p>Cruise in the best car brands without the bloated prices Enjoy the enhanced comfort of a luxury rental and arrive in style.</p>
+                        <p className="text-[12px]">Cruise in the best car brands without the bloated prices Enjoy the enhanced comfort of a luxury rental and arrive in style.</p>
 
                     </div>
 
-                    <footer>
+                    <footer className="pt-10">
 
-                        <button>Learn More</button>
+                        <button className="py-2 px-5 rounded-full text-[12px]">Learn More</button>
 
                     </footer>
 
